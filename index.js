e2db9ac228910489827e661b7973e424fcbb892b
Enzofromspace <8026658+Enzofromspace@users.noreply.github.com>
2023-01-03 23:20:32 +0300

    Setup a loop that lets shift key select multiple boxes

--- index.js
+++ index.js
@@ -4,3 +4,29 @@ import './style.css';
 // Write Javascript code!
 const appDiv = document.getElementById('app');
 appDiv.innerHTML = `<h1>Check Multiple Boxes with JS</h1>`;
+
+const checkboxes = document.querySelectorAll('.inbox input[type="checkbox"]');
+
+let lastChecked;
+
+function handleCheck(e){
+  //console.log(e);
+  //check for shift key
+  let inBetween = false;
+  if(e.shiftKey && this.checked){
+    checkboxes.forEach(checkbox => {
+      //console.log(checkbox);
+      if(checkbox === this || checkbox === lastChecked){
+        inBetween = !inBetween;
+      }
+      if(inBetween){
+        checkbox.checked = true;
+      }
+    });
+  }
+    lastChecked = this;
+}
+
+checkboxes.forEach(checkbox => checkbox.addEventListener('click', handleCheck));
+
+//console.log(checkboxes);
